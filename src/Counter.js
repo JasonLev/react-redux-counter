bab1b397b2d01f843faa1512cf57e3368d354693
JasonLev <jasond.levy@gmail.com>
2018-11-14 18:00:34 -0800

    f/reducer refactor, dispatch func

--- src/Counter.js
+++ src/Counter.js
@@ -3,12 +3,25 @@ import { connect } from 'react-redux';
 
 
 class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  increment() {
+    this.props.dispatch({type: 'INCREMENT'})
+  }
+  decrement() {
+    this.props.dispatch({type: 'DECREMENT'})
+  }
   render() {
     return (
       <main>
-        <button>&ndash;</button>
-        <span className="count">{this.props.count}</span>
-        <button>+</button>
+        <button onClick={this.decrement}>&ndash;</button>
+        <span className={this.props.count < 0 ? 'count red': 'count'}>
+          {this.props.count}
+        </span>
+        <button onClick={this.increment}>+</button>
       </main>
     );
   }
